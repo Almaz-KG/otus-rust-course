a412464b946bd1813031546a4a40c4bb1c31def8
Almaz <almaz@murzabekov.net>
2022-12-09 01:09:47 +0400

    [hw-004-enum-based] Enum based implementation of HW-004

---
+++ projects/hw-004-enum-based/src/entities/devices/device.rs
@@ -0,0 +1,48 @@
+use super::socket::Socket;
+use super::thermometer::Thermometer;
+use crate::entities::Reportable;
+use std::fmt::{Display, Formatter, Result};
+
+/// This is a common Trait representing variety of devices in Smart Home project. Currently it
+/// has only two elements, but it the future implementation it may have more.
+///
+/// There are two approaches how to handle problem of storing dynamically typed elements in
+/// one typed container. There is no any java-like `Object` classes, and runtime at all.
+///
+/// The first approach is to use `dyn MyTrait` type as element type of container. It means,
+/// developer will be forced to implement `MyTrait` trait for each device. And the main drawback
+/// of this approach is the type erasure. Users of the `dyn MyTrait` will not have an access for
+/// the fields and methods for the type behind the `dyn MyTrait` type. Instead, all internal
+/// features will be hidden by Trait public API, which will lead to down-cast the trait.
+///
+/// Another approach, and this is the implementation of this approach, is to have enum for
+/// each [Device] in the project. It's much easier to deal with enum, which incapsulates each
+/// device types within the element of Enum. A negative side of this approach is that this enum
+/// does not support easy inheritance. If user want to introduce a new device type to the
+/// project, he/she will forced to update this enum, as well as update all places where this
+/// enum was used, it's not cool at all. A classical `composition` pattern might be considered as
+/// one of possible solution for the `extension` limitation
+#[derive(Debug)]
+pub enum Device {
+    Socket(Socket),
+    Thermometer(Thermometer),
+}
+
+impl Display for Device {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        match self {
+            Device::Socket(s) => formatter.write_str(&format!("{s}")),
+
+            Device::Thermometer(t) => formatter.write_str(&format!("{t}")),
+        }
+    }
+}
+
+impl Reportable for Device {
+    fn report(&self) -> String {
+        match self {
+            Device::Socket(s) => s.report(),
+            Device::Thermometer(t) => t.report(),
+        }
+    }
+}
---
+++ projects/hw-004-enum-based/src/main.rs
@@ -0,0 +1,61 @@
+use hw_004_enum_based::entities::devices::{Device, Socket, Thermometer};
+use hw_004_enum_based::entities::house::{Home, Room};
+use hw_004_enum_based::entities::Reportable;
+
+fn main() {
+    let house = Home::build()
+        .with_name("Home #1")
+        .with_description("Moscow's home")
+        .with_room(
+            Room::build()
+                .with_name("Living room")
+                .with_description("Living room with 48 sq meter size")
+                .with_device(Device::Socket(Socket::new(
+                    "Main light socket",
+                    "Located near the entry door",
+                )))
+                .with_device(Device::Socket(Socket::new(
+                    "Second light socket",
+                    "Located near the window",
+                )))
+                .build()
+                .expect("Unable to create the living room"),
+        )
+        .with_room(
+            Room::build()
+                .with_name("Kitchen")
+                .with_description("The kingdom of my wife")
+                .with_device(Device::Socket(Socket::new(
+                    "The light socket",
+                    "Located at the entry door",
+                )))
+                .with_device(Device::Thermometer(Thermometer::new(
+                    "A thermometer behind the window",
+                    "Super old mercury thermometer",
+                )))
+                .build()
+                .expect("Unable to create the kitchen"),
+        )
+        .build()
+        .expect("Unable to create a smart house");
+
+    println!("=========== Display ===============");
+    println!("=========== Home ===============");
+    println!("{}", house);
+
+    println!("=========== Rooms ===============");
+    let rooms = house.get_rooms();
+    for room in rooms {
+        println!("{}", room);
+    }
+    println!("=========== Devices =============");
+    let devices = house
+        .get_devices(&rooms[0])
+        .expect("Unable to get devices list");
+    for device in devices {
+        println!("{}", device);
+    }
+
+    println!("=========== Report ===============");
+    println!("{}", house.report());
+}

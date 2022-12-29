87c242a8598065bc7b82ddc356a04bcbef83baa8
Almaz <almaz@murzabekov.net>
2022-12-29 11:55:04 +0400

    [hw-006] initial commit

---
+++ projects/hw-006/src/entities/devices/thermometer.rs
@@ -0,0 +1,62 @@
+use crate::entities::reportable::{ReportError, Reportable};
+use crate::entities::{Measure, MeasureError};
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+/// A super short and stupid wrapper around the Thermometer entity. In theory and the future
+/// implementations, it should become more robust and meaningful entity with variety internal
+/// states and functionality. As for now, it's just a dummy thermometer
+#[derive(Debug)]
+pub struct Thermometer {
+    name: String,
+    description: Option<String>,
+}
+
+/// A thermometer struct implementation, it mostly wrapper and dummy stub-logic inside each method.
+impl Thermometer {
+    /// Creates a new instance of [Thermometer] by given name and description. It's a fake
+    /// struct, because in the real life we don't need the name of our thermometer.
+    pub fn new(name: &str, description: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            description: Some(description.to_string()),
+        }
+    }
+}
+
+/// A super straight forward implemntation of the [Display] trait for Thermometer struct. It
+/// writes out the name and the description of the instance with the `Thermometer` prefix.
+impl Display for Thermometer {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FmtResult {
+        let txt = format!(
+            "Thermometer: {}, {}",
+            self.name,
+            self.description.clone().unwrap_or_default()
+        );
+
+        write!(formatter, "{}", txt)
+    }
+}
+
+impl Measure<f32> for Thermometer {
+    /// A fake implementation of the `measure` function for the given thermometer instance. The
+    /// current implementation gives a random number from [0.. 1.0). I don't expect that the logic
+    /// behind this method will be changed in scope of this project at all. So, please consider
+    /// to use it if you want to get some random number in your use case
+    fn measure(&self) -> Result<Option<f32>, MeasureError> {
+        Ok(Some(rand::random()))
+    }
+}
+
+/// A Reportable implementation for Thermometer struct fives back for caller the String
+/// representation of the current status. It makes measurement for building the status report.
+impl Reportable for Thermometer {
+    fn report(&self) -> Result<String, ReportError> {
+        match self.measure() {
+            Ok(result) => match result {
+                Some(value) => Ok(format!("Thermometer: {}, Measure: {}", self.name, value)),
+                None => Ok(format!("Thermometer: {}, No measure value", self.name)),
+            },
+            Err(msg) => Err(msg.into()),
+        }
+    }
+}
---
+++ projects/hw-006/src/lib.rs
@@ -0,0 +1,15 @@
+//! This is a container for different variety of entities
+//! for the OTUS smart-home project.
+//!
+//! Smart home project is a series of small rust logics
+//! aiming to provide the complete smart house, with variety
+//! smart controllers and devices.
+//!
+//! The `entities` sub module contains as name claims the entities
+//! for the project use case.
+//!
+//! Hopefully, in the future home works, a bunch of new submodules
+//! will be included here, so this module will not be so lonely
+//!
+
+pub mod entities;
